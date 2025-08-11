1ecb0521f1e88df8afd47e9e1cf2a4e17f7ab513
mfaik <m.faikekclr@gmail.com>
2025-08-11 15:13:53 +0900

    added recursive drawing, and removed parent from the box

--- boxes/sketch.js
+++ boxes/sketch.js
@@ -12,7 +12,7 @@ let increase_tick = () => tick++;
 //one may ask, > why is this type named "bx" and not "box"
 //> are you saving the world with the time you saved not typing "o"?
 //but unfortunately, this change was made to avoid naming conflict with p5.box()
-/** @typedef {{ x: number, y: number, parent: bx, children: bx[], last_movement_tick: number}} bx */
+/** @typedef {{ x: number, y: number, children: bx[], last_movement_tick: number}} bx */
 /** 
  * @param {bx} parent
  * @param {number} x
@@ -45,31 +45,38 @@ let push_camera = (selected_box) => {
     camera_stack.push(c);
     return c;
 };
+let pop_camera = () => {
+    camera_stack.pop();
+}
 
+let get_camera_depth = () => {
+    return camera_stack.length;
+}
 let get_current_camera = () => 
     camera_stack[camera_stack.length-1];
 let get_main_box = () => 
     camera_stack[0].selected_box;
 let get_current_box = () => 
     camera_stack[camera_stack.length-1].selected_box;
+/** @param {number} i */
+let get_box_at_depth = (i) => 
+    camera_stack[i].selected_box;
 /** 
  * @param {number} x
  * @param {number} y
  */
 let push_box_current = (x, y) => 
-    create_box(x, y, get_current_camera().selected_box);
+    create_box(x, y, get_current_box());
 /**
  * @param {number} x
  * @param {number} y
  */
 let get_box_current = (x, y) => 
-    get_box(x, y, get_current_camera().selected_box)
+    get_box(x, y, get_current_box())
 
 /** @param {bx} box */
 let wipe_box = (box) => {
-    //using delete is fine, as this is done to satisfy gc
-    /** @ts-ignore */
-    delete box.parent;
+    box.x = Infinity;
     for(let c of box.children) {
         wipe_box(c)
     }
@@ -77,14 +84,14 @@ let wipe_box = (box) => {
 }
 /** 
  * @param {bx} box
+ * @param {bx} parent
  * @param {boolean} [undoable]
  */
-let remove_box = (box, undoable = false) => {
-    let p = box.parent;
-    let i = p.children.indexOf(box);
+let remove_box = (box, parent, undoable = false) => {
+    let i = parent.children.indexOf(box);
     if(i != -1) {
-        p.children[i] = p.children[p.children.length-1];
-        p.children.pop();
+        parent.children[i] = parent.children[parent.children.length-1];
+        parent.children.pop();
     } else {
         throw new Error("Trying to remove a box that does not exists!");
     }
@@ -93,23 +100,24 @@ let remove_box = (box, undoable = false) => {
 }
 /** 
  * @param {bx} box 
+ * @param {bx} old_parent
  * @param {bx} new_parent
  */
-let change_parent = (box, new_parent) => {
-    let p = box.parent;
-
-    let i = p.children.indexOf(box);
+let change_parent = (box, old_parent, new_parent) => {
+    let i = old_parent.children.indexOf(box);
     if(i == -1) throw new Error("Internal error, orphan box");
-    p.children[i] = p.children[p.children.length-1];
-    p.children.pop();
+    old_parent.children[i] = old_parent.children[old_parent.children.length-1];
+    old_parent.children.pop();
 
-    box.parent = new_parent; 
     new_parent.children.push(box);
 }
-/** @param {bx} box */
-let decrease_degree = (box) => {
+/** 
+ * @param {bx} box 
+ * @param {bx} parent 
+ */
+let decrease_degree = (box, parent) => {
     if(box.children.length == 0) {
-        remove_box(box);
+        remove_box(box, parent);
         return;
     }
     let destroy = box.children[0];
@@ -119,7 +127,7 @@ let decrease_degree = (box) => {
             continue;
         }
     }
-    remove_box(destroy);   
+    remove_box(destroy, box);   
 }
 /** 
  * @param {number} width
@@ -153,8 +161,9 @@ let normalize_box = (box) => {
 /**
  * @param {bx} base_box
  * @param {bx} applied_box
+ * @param {bx} parent
  */
-let expand_box = (base_box, applied_box) => {
+let expand_box = (base_box, applied_box, parent) => {
     //shape detection TODO make this better
     if(base_box.children.length != 4) return false;
     let l = base_box.children[0];
@@ -182,22 +191,28 @@ let expand_box = (base_box, applied_box) => {
         (base_box.y-applied_box.y)/2,
         width, height);
     for(let c of applied_box.children) {
-        c.parent = base_box.parent;
-        base_box.parent.children.push(c);
         c.x += offset.x;
         c.y += offset.y;
+        let b = get_box(c.x, c.y, parent);
+        parent.children.push(c);
+        if(b) {
+            c.x -= base_box.x-applied_box.x;
+            c.y -= base_box.y-applied_box.y;
+            apply_box(b, c, parent);
+        }
     }
     applied_box.children.length = 0;
-    remove_box(applied_box);
-    remove_box(base_box);
+    remove_box(applied_box, parent);
+    remove_box(base_box, parent);
 
     return true;
 }
 /**
  * @param {bx} base_box
  * @param {bx} applied_box
+ * @param {bx} parent
  */
-let compact_box = (base_box, applied_box) => {
+let compact_box = (base_box, applied_box, parent) => {
     //shape detection TODO: make this better
     if(base_box.children.length != 5) return false;
     let l = base_box.children[0];
@@ -225,27 +240,27 @@ let compact_box = (base_box, applied_box) => {
     let offset_y = (base_box.y-applied_box.y)/2;
     let area_offset = calculate_area_offset(base_box.x, base_box.y, 
                                             offset_x, offset_y, width, height);
-    let new_box = create_box(Infinity, Infinity, base_box.parent);
+    let new_box = create_box(Infinity, Infinity, parent);
     for(let c of applied_box.children) {
-        let b = get_box(c.x + area_offset.x, c.y + area_offset.y, base_box.parent);
+        let b = get_box(c.x + area_offset.x, c.y + area_offset.y, parent);
         if(b) {
             b.x -= area_offset.x;
             b.y -= area_offset.y;
-            change_parent(b, new_box);
+            change_parent(b, parent, new_box);
         }
     }
 
     applied_box.children.length = 0;
-    remove_box(applied_box);
-    remove_box(base_box);
+    remove_box(applied_box, parent);
+    remove_box(base_box, parent);
 
     let new_pos_x = base_box.x+offset_x;
     let new_pos_y = base_box.y+offset_y;
-    let block = get_box(new_pos_x, new_pos_y, new_box.parent);
+    let block = get_box(new_pos_x, new_pos_y, parent);
     if(block) {
         new_box.x = base_box.x-offset_x;
         new_box.y = base_box.y-offset_y;
-        apply_box(block, new_box);
+        apply_box(block, new_box, parent);
     } else {
         new_box.x = new_pos_x;
         new_box.y = new_pos_y;
@@ -256,28 +271,19 @@ let compact_box = (base_box, applied_box) => {
 /**
  * @param {bx} base_box
  * @param {bx} applied_box
+ * @param {bx} parent
  */
-let apply_box = (base_box, applied_box) => {
-    if(expand_box(base_box, applied_box)) return;
-    if(compact_box(base_box, applied_box)) return;
-    for(let c of applied_box.children) {
-        //we need to set the parent to undefined to make sure that, 
-        //the parent(applied_box) is not endlessly copied
-        //this line won't invalidate types because later we will be setting the parent
-        /** @ts-ignore */
-        c.parent = undefined;
-    }
+let apply_box = (base_box, applied_box, parent) => {
+    if(expand_box(base_box, applied_box, parent)) return;
+    if(compact_box(base_box, applied_box, parent)) return;
     for(let c of base_box.children) {
         if(c.children.length == 0) {
             c.children = structuredClone(applied_box.children);
-            for(let cc of c.children) {
-                cc.parent = c;
-            }
         } else {
-            decrease_degree(c);
+            decrease_degree(c, base_box);
         }
     }
-    remove_box(applied_box);
+    remove_box(applied_box, parent);
 }
 
 const UndoTypes = {
@@ -302,9 +308,10 @@ let push_undo_camera_push = (box) => {
 }
 /** 
  * @param {bx} box
+ * @param {bx} parent
  */
-let push_undo_box_remove = (box) => {
-    undo_stack.push({type: UndoTypes.BOX_REMOVE, box});
+let push_undo_box_remove = (box, parent) => {
+    undo_stack.push({type: UndoTypes.BOX_REMOVE, box, parent});
 }
 /** 
  * @param {bx} box
@@ -322,9 +329,9 @@ let undo = () => {
     if(undo_obj.type == UndoTypes.CAMERA_PUSH) {
         push_camera(undo_obj.box);
     } else if(undo_obj.type == UndoTypes.CAMERA_POP) {
-        camera_stack.pop();
+        pop_camera();
     } else if(undo_obj.type == UndoTypes.BOX_REMOVE) {
-        remove_box(undo_obj.box);
+        remove_box(undo_obj.box, undo_obj.parent);
     } else if(undo_obj.type == UndoTypes.BOX_ADD) {
         undo_obj.parent.children.push(undo_obj.box);
     }
@@ -348,7 +355,6 @@ let load_game = () => {
 }
 
 //this first box will be the context that will be used for the entire game
-//also this will be the only box to allow having undefined as parent
 /** @type {bx} */
 
 /** @ts-ignore */
@@ -358,22 +364,20 @@ window.setup = function() {
     /** @type bx */
     let main_box = {
         x: 0, y: 0, 
-        /** @ts-ignore*/
-        parent: undefined, 
-        children: []
+        children: [],
+        last_movement_tick: -1,
     };
     push_camera(main_box);
 }
 
-/** @type {Record<string | number, boolean>} */
-let pressed_keys = {};
 let logic_timer = 0;
 /** @ts-ignore */
 window.draw = function() {
-    background(30+camera_stack.length*10, 10, 10);
+    background(30+get_camera_depth()*10, 10, 10);
 
-    for(let i = 0;i < camera_stack.length-1;i++) {
-        rect(5+i*20, 5, 15);
+    for(let i = 0;i < get_camera_depth()-1;i++) {
+        draw_box_at_place(i+1, 1, 20, 2, get_box_at_depth(i));
+        //rect(5+i*20, 5, 15);
     }
     logic_timer += deltaTime;
     while(logic_timer > logic_interval) {
@@ -384,10 +388,10 @@ window.draw = function() {
 
     let c = get_current_camera();
 
-    if(pressed_keys[RIGHT_ARROW]) c.x -= c.scale*deltaTime;
-    if(pressed_keys[LEFT_ARROW]) c.x += c.scale*deltaTime;
-    if(pressed_keys[DOWN_ARROW]) c.y -= c.scale*deltaTime;
-    if(pressed_keys[UP_ARROW]) c.y += c.scale*deltaTime;
+    if(keyIsDown(RIGHT_ARROW)) c.x -= c.scale*deltaTime;
+    if(keyIsDown(LEFT_ARROW)) c.x += c.scale*deltaTime;
+    if(keyIsDown(DOWN_ARROW)) c.y -= c.scale*deltaTime;
+    if(keyIsDown(UP_ARROW)) c.y += c.scale*deltaTime;
 
     translate(c.x, c.y)
     scale(c.scale)
@@ -399,36 +403,39 @@ window.draw = function() {
 
 let game_logic = () => {
     increase_tick();
-    /** @type {bx[]} */
+    /** @type {{box: bx, parent: bx}[]} */
     let box_queue = []
     for(let c of get_main_box().children)
-        box_queue.push(c)
+        box_queue.push({box: c, parent: get_main_box()})
     let head = 0;
     while(box_queue.length-head) {
         let b = box_queue[head++];
-        box_check(b)
-        for(let c of b.children)
-            box_queue.push(c)
+        box_check(b.box, b.parent)
+        for(let c of b.box.children)
+            box_queue.push({box: c, parent:b.box})
     }
 
-    while(!get_current_box() || (get_current_box() != get_main_box() && !get_current_box().parent))
-        camera_stack.pop();
+    while(!get_current_box() || (get_current_box() != get_main_box() && get_current_box().x == Infinity))
+        pop_camera();
 }
-
 
 /** 
  * @param {number} dx 
  * @param {number} dy 
  * @param {bx} box 
+ * @param {bx} parent 
  */
-let try_move = (dx, dy, box) => {
-    let moved = move_box(dx, 0, box);//l
-    moved = move_box(0, dy, box) || moved;//u
-    if(moved)remove_box(box);
+let try_move = (dx, dy, box, parent) => {
+    let moved = move_box(dx, 0, box, parent);//l
+    moved = move_box(0, dy, box, parent) || moved;//u
+    if(moved)remove_box(box, parent);
     return moved;
 }
-/** @param {bx} box */
-let box_check = (box) => {
+/** 
+ * @param {bx} box 
+ * @param {bx} parent
+ */
+let box_check = (box, parent) => {
     if(box.children.length != 3) return;
     for(let anchor = 0;anchor < 3;anchor++) {
         //shape detection
@@ -443,29 +450,30 @@ let box_check = (box) => {
         let d = (a.x == b.x && a.y == b.y+1) || (a.x == c.x && a.y == c.y+1);
         //movement
         if(l && u)
-            if(try_move(1, -1, box)) return;
+            if(try_move(1, -1, box, parent)) return;
         if(r && u)
-            if(try_move(-1, -1, box)) return;
+            if(try_move(-1, -1, box, parent)) return;
         if(l && d)
-            if(try_move(1, 1, box)) return;
+            if(try_move(1, 1, box, parent)) return;
         if(r && d)
-            if(try_move(-1, 1, box)) return;
+            if(try_move(-1, 1, box, parent)) return;
     }
 }
 /**
  * @param {number} x_offset
  * @param {number} y_offset
  * @param {bx} box
+ * @param {bx} parent
  * @return boolean
  */
-let move_box = (x_offset, y_offset, box) => {
-    let from_box = get_box(box.x+x_offset, box.y+y_offset, box.parent);
+let move_box = (x_offset, y_offset, box, parent) => {
+    let from_box = get_box(box.x+x_offset, box.y+y_offset, parent);
     if(!from_box || from_box.last_movement_tick == get_current_tick())
         return false;
 
-    let to_box = get_box(box.x-x_offset, box.y-y_offset, box.parent); 
+    let to_box = get_box(box.x-x_offset, box.y-y_offset, parent); 
     if(to_box) {
-        apply_box(to_box, from_box);
+        apply_box(to_box, from_box, parent);
     } else {
         from_box.x -= x_offset*2;
         from_box.y -= y_offset*2;
@@ -475,12 +483,46 @@ let move_box = (x_offset, y_offset, box) => {
     return true;
 }
 
-/** @param {bx} box */
-function draw_box(box) {
-    rect((box.x)*default_tile_size+2, 
-        (box.y)*default_tile_size+2, default_tile_size-4);
+/** 
+ * @param {number} x
+ * @param {number} y
+ * @param {number} default_size
+ * @param {number} default_offset
+ * @param {bx} box
+ */
+let draw_box_at_place = (x, y, default_size, default_offset, box) => {
+    rect(x*default_size+default_offset, 
+         y*default_size+default_offset, default_size-default_offset*2);
+    if(box.children.length == 0)return;
+    let l = box.children[0].x, u = box.children[0].y;
+    let r = l, d = u;
+    for(let c of box.children) {
+        if(c.x < l)l = c.x;
+        if(c.x > r)r = c.x;
+        if(c.y < u)u = c.y;
+        if(c.y > d)d = c.y;
+    }
+    let box_default_size = (default_size-default_offset*4)/(d-u+1);
+    if(d-u>r-l) {
+        l -= ((d-u)-(r-l))/2;
+    }
+    if(d-u<r-l) {
+        u -= ((r-l)-(d-u))/2;
+        box_default_size = (default_size-default_offset*4)/(r-l+1);
+    }
+    fill(30, 90, 200);
+    for(let c of box.children) {
+        rect((c.x-l)*box_default_size+(x*default_size)+default_offset*2, 
+             (c.y-u)*box_default_size+(y*default_size)+default_offset*2, 
+             max(2, ceil(box_default_size)));
+    }
+    fill(255);
 }
 
+/** @param {bx} box */
+let draw_box = (box) => {
+    draw_box_at_place(box.x, box.y, default_tile_size, 2, box);
+}
 
 window.mousePressed = function() {
     let current_camera = get_current_camera();
@@ -498,13 +540,13 @@ window.mousePressed = function() {
             push_undo_camera_pop();
         } else {
             let b = push_box_current(x, y)
-            push_undo_box_remove(b);
+            push_undo_box_remove(b, get_current_box());
         }
     } else if(mouseButton.right) {
         let b = get_box_current(x, y);
         if(b) {
-            push_undo_box_add(b, b.parent);
-            remove_box(b, true)
+            push_undo_box_add(b, get_current_box());
+            remove_box(b, get_current_box(), true)
         }
     }
 }
@@ -519,12 +561,10 @@ window.mouseWheel = function(event) {
 }
 
 window.addEventListener('keydown', (e) => {
-    pressed_keys[e.key] = true;
-
     if(e.key == 'Escape' || e.key == 'e') {
-        if(camera_stack.length > 1) {
+        if(get_camera_depth() > 1) {
             push_undo_camera_push(get_current_box());
-            camera_stack.pop();
+            pop_camera();
         }
     } else if(e.key == 's') {
         save_game();
@@ -534,14 +574,6 @@ window.addEventListener('keydown', (e) => {
     } else if(e.key == 'z') {
         undo();
     }
-});
-
-window.addEventListener('keyup', (e) => {
-  pressed_keys[e.key] = false;
-});
-
-window.addEventListener('blur', () => {
-  pressed_keys = {};
 });
 
 window.windowResized = function() {
